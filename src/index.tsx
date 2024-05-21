9901cd7f7516e1268bea2b106d42d5422514bd50
Александр Жилкин <a.zhilkin@st-falcon.ru>
2024-05-21 15:44:02 -0700

    deleted dependecies

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BaseCSS } from 'styled-bootstrap-grid'
 import { App } from './App'
-import { Provider as MobxProvider } from 'mobx-react'
-
-import * as stores from './stores'
 
 ReactDOM.render(
   <>
-    <MobxProvider {...stores}>
-      <BaseCSS />
-      <App />
-    </MobxProvider>
+    <BaseCSS />
+    <App />
   </>,
   document.getElementById('root'),
 )
--- src/stores/utils.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { MobXProviderContext } from 'mobx-react'
-
-export function useStores() {
-  return useContext(MobXProviderContext)
-}
